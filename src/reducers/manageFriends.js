801a862c1636913ec28080671e851f50d081a8c2
danielkwon89 <danielkwon89@gmail.com>
2021-01-27 13:12:10 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,21 @@
 export function manageFriends(state, action){
+    switch (action.type) {
+        
+        case "ADD_FRIEND":
+            let newObj = Object.assign({}, state)
+            newObj.friends.push(action.friend)
+            return newObj
+
+        case "REMOVE_FRIEND":
+            let newObject = Object.assign({}, state)
+            newObject.friends.map((friend, index) => {
+                if (friend.id === action.id) {
+                    newObject.friends.splice(index, 1)
+                }
+            })
+            return newObject
+
+        default: 
+            return state
+    }
 }
